e8203cecec8dd05137efc14b1dba5f8c7e11aabb
Tim Van Ginderen <timvanginderen@gmail.com>
2023-10-22 20:47:42 +0200

    Add stepper widget

--- lib/screens/order/order_screen.dart
+++ lib/screens/order/order_screen.dart
@@ -17,6 +17,80 @@ class _OrderScreenState extends ConsumerState<OrderScreen> {
         final OrderViewModel orderViewModel = ref.watch(orderViewModelProvider);
         return Scaffold(
           appBar: AppBar(title: const Text('Order')),
+          body: Container(
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Stepper(
+                    type: StepperType.horizontal,
+                    physics: const ScrollPhysics(),
+                    currentStep: orderViewModel.currentStep,
+                    onStepTapped: (int step) => orderViewModel.tapped(step),
+                    onStepContinue: orderViewModel.continued,
+                    onStepCancel: orderViewModel.cancelled,
+                    controlsBuilder:
+                        (BuildContext context, ControlsDetails details) {
+                      return Row(
+                        children: <Widget>[
+                          if (orderViewModel.currentStep > 0)
+                            ElevatedButton(
+                              onPressed: details.onStepCancel,
+                              child: const Text('BACK'),
+                            ),
+                          const Spacer(),
+                          ElevatedButton(
+                            onPressed: details.onStepContinue,
+                            child: const Text('NEXT'),
+                          ),
+                        ],
+                      );
+                    },
+                    steps: <Step>[
+                      Step(
+                        title: const Text('Details'),
+                        content: Column(
+                          children: <Widget>[
+                            TextFormField(
+                              decoration:
+                                  const InputDecoration(labelText: 'Name'),
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                  labelText: 'Email Address'),
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                  labelText: 'Phone number'),
+                            ),
+                          ],
+                        ),
+                        isActive: orderViewModel.currentStep >= 0,
+                        state: orderViewModel.currentStep >= 0
+                            ? StepState.complete
+                            : StepState.disabled,
+                      ),
+                      Step(
+                        title: const Text('Pricing'),
+                        content: const Column(),
+                        isActive: orderViewModel.currentStep >= 0,
+                        state: orderViewModel.currentStep >= 1
+                            ? StepState.complete
+                            : StepState.disabled,
+                      ),
+                      Step(
+                        title: const Text('Summary'),
+                        content: const Column(),
+                        isActive: orderViewModel.currentStep >= 0,
+                        state: orderViewModel.currentStep >= 2
+                            ? StepState.complete
+                            : StepState.disabled,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         );
       },
     );
--- lib/screens/order/order_view_model.dart
+++ lib/screens/order/order_view_model.dart
@@ -12,6 +12,10 @@ final AutoDisposeChangeNotifierProvider<OrderViewModel> orderViewModelProvider =
 
 abstract class OrderViewModel extends ViewModel {
   Future<void> initState();
+  int get currentStep;
+  void continued();
+  void cancelled();
+  void tapped(int step);
 }
 
 @Injectable(as: OrderViewModel)
@@ -19,7 +23,33 @@ class OrderViewModelImpl extends BaseViewModel implements OrderViewModel {
   OrderViewModelImpl(this.navigationService);
 
   final NavigationService navigationService;
+  int _currentStep = 0;
 
   @override
   Future<void> initState() async {}
+
+  @override
+  int get currentStep => _currentStep;
+
+  @override
+  void continued() {
+    _currentStep < 2 ? _currentStep += 1 : finishForm();
+    notifyListeners();
+  }
+
+  @override
+  void cancelled() {
+    if (_currentStep > 0) {
+      _currentStep -= 1;
+    }
+    notifyListeners();
+  }
+
+  @override
+  void tapped(int step) {
+    _currentStep = step;
+    notifyListeners();
+  }
+
+  void finishForm() {}
 }
